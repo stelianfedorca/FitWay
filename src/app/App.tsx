eff981ebfe69a668fd05c5051cf19f2e199f6eab
Stelian Fedorca <stelian.fedorca25@gmail.com>
2022-11-27 21:20:47 +0200

    Moved the App.tsx file

---
+++ src/app/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+export function App() {
+  return (
+    <View style={styles.container}>
+      <Text>asdsdasads</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
